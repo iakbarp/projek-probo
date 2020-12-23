c8cccf166a7c4003279e29a053ef2ee3f9330f4b
iakbarp <akbardoto@gmail.com>
2020-12-23 17:09:08 +0700

    konfirmasi admin pembayara

--- database/migrations/2020_12_21_091931_create_saldo_view.php
+++ database/migrations/2020_12_21_091931_create_saldo_view.php
@@ -14,14 +14,33 @@ class CreateSaldoView extends Migration
      */
     public function up()
     {
-        DB::statement("create view view_saldo as SELECT id, topup-withdraw-pembayaran-pembayaran_layanan saldo from (
-            SELECT u.id,
-        (select ifnull(sum(t.jumlah),0) from topup as t where t.user_id=u.id) as topup,
-        (select ifnull(sum(w.jumlah),0) from withdraw as w where w.user_id=u.id and w.konfirmasi=1)  withdraw,
-        (select ifnull(sum(p.jumlah_pembayaran),0) from pembayaran as p join project pr on pr.id=p.proyek_id and  p.isDompet=1 WHERE pr.user_id=u.id) pembayaran,
-        (select ifnull(sum(pl.jumlah_pembayaran),0) from pembayaran_layanan as pl join pengerjaan_layanan pl2 on pl2.id=pl.pengerjaan_layanan_id and  pl.isDompet=1 where pl2.user_id=u.id) pembayaran_layanan
-
-                from users as u) a");
+        DB::statement("create view view_saldo as SELECT `a`.`id`
+       AS
+       `id`,
+       `a`.`topup` - `a`.`withdraw` - `a`.`pembayaran` -
+       `a`.`pembayaran_layanan` AS
+       `saldo`
+        FROM   (SELECT `u`.`id`                            AS `id`,
+               (SELECT Ifnull(Sum(`t`.`jumlah`), 0)
+                FROM   `undagimy_db`.`topup` `t`
+                WHERE  `t`.`user_id` = `u`.`id` and konfirmasi=1)   AS `topup`,
+               (SELECT Ifnull(Sum(`w`.`jumlah`), 0)
+                FROM   `undagimy_db`.`withdraw` `w`
+                WHERE  `w`.`user_id` = `u`.`id`
+                       AND `w`.`konfirmasi` = 1)   AS `withdraw`,
+               (SELECT Ifnull(Sum(`p`.`jumlah_pembayaran`), 0)
+                FROM   (`undagimy_db`.`pembayaran` `p`
+                        JOIN `undagimy_db`.`project` `pr`
+                          ON( `pr`.`id` = `p`.`proyek_id`
+                              AND `p`.`isdompet` = 1 ))
+                WHERE  `pr`.`user_id` = `u`.`id`)  AS `pembayaran`,
+               (SELECT Ifnull(Sum(`pl`.`jumlah_pembayaran`), 0)
+                FROM   (`undagimy_db`.`pembayaran_layanan` `pl`
+                        JOIN `undagimy_db`.`pengerjaan_layanan` `pl2`
+                          ON( `pl2`.`id` = `pl`.`pengerjaan_layanan_id`
+                              AND `pl`.`isdompet` = 1 ))
+                WHERE  `pl2`.`user_id` = `u`.`id`) AS `pembayaran_layanan`
+        FROM   `undagimy_db`.`users` `u`) `a`");
     }
 
     /**
